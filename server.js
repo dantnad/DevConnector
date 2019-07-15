22970c8513d790cd5d14dc072a2f2263c557d320
Daniel Contreras <daniel@hibuddie.com>
2019-07-15 15:57:22 -0500

    Configured app routes and created user schema

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
 
-const PORT = process.env.PORT || 5000;
+// Defime Routes
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 // Connect to Database
 connectDB();
 
-app.get('/', (req, res) => { res.send('API Running') })
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => { console.log(`Server started on port ${PORT}`)});
