7916989ca14846e43c77fbc8336acbc58d62d7ca
Daniel Contreras <daniel@hibuddie.com>
2019-07-15 16:41:01 -0500

    Added express validator and error messages to user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator')
 
 //@route    GET api/users
-//@dest     Test route
+//@dest     Register User
 //@access   Public
-router.get('/', (req, res)=>{
-  res.send('User Route')
-});
+router.post('/', [
+    check('name', 'name is required').not().isEmpty(),
+    check('email', 'Please include an email').isEmail(),
+    check('password', 'Please enter a password of 6 or more characters').isLength({ min: 6 })
+  ],
+  (req, res)=>{
+      console.log(req.body);
+      const errors = validationResult(req);
+      if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      res.send('User Route')
+    }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,15 +2,18 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
 
+// Connect to Database
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
 // Defime Routes
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
-
-// Connect to Database
-connectDB();
 
 const PORT = process.env.PORT || 5000;
 
